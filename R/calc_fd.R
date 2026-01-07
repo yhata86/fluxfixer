6f32e278de8b5b8f250d8b51f0c86264a0deab35
yhata86 <yoshiaki.hata0806@gmail.com>
2026-01-07 19:57:33 +0900

    added explanation

--- R/calc_fd.R
+++ R/calc_fd.R
@@ -1,34 +1,59 @@
-#' Calculate Fd from dT and dTmax time series
+#' Calculate sap flux density time series
 #'
-#' @description calc_fd() calculates sap flow rate using the Granier equation
-#'   from the dT and dTmax time series, considering heartwood correction.
+#' @description `calc_fd()` calculates Fd (sap flux density) time series by a
+#'  power-type function, including heartwood correction.
 #'
-#' @param vctr_dt Vector of dT (the temperature difference between sap flow
-#'   probes) time series. Missing values must be gap-filled previously.
-#' @param vctr_dtmax Vector of the dTmax (the maximum temperature difference
-#'   between sap flow probes) time series estimated by a specific method.
-#' @param alpha Parameter in Granier equation (unit: m/s)
-#' @param beta Parameter in Granier equation (dimensionless)
-#' @param do_heartwood_correction Logical. Do Heartwood correction described in
-#'   Clearwater et al. (1999) or not?
-#' @param ratio_conductive Ratio of conductive versus non-conductive length
-#'   (dimensionless)
+#' @details Fd is estimated using a power-type function introduced by Granier
+#'  (1985; Annales des Sciences Forestieres, 1987; Tree Physiology). First, a
+#'  dimensionless index K is obtained from dT and dTmax. Second, K is raised to
+#'  the power `beta` and then multiplied by `alpha`, obtaining Fd.
+#'
+#'  If the sapwood width is shorter than the probe insertion length, dT can be
+#'  overestimated, resulting in an underestimation of Fd. Therefore, heartwood
+#'  correction is required to correct dT. Optionally, before calculating Fd, dT
+#'  can be replaced with the corrected dT by specifying the ratio of the probe
+#'  length to sapwood width. This correction assumes that the dT measured by
+#'  the part of the probe that is inserted into the heartwood is always dTmax.
+#'  See more details in Clearwater et al. (1999; Tree Physiology).
+#'
+#' @inheritParams calc_dtmax_sp
+#' @param vctr_dtmax A vector of dTmax (the maximum temperature difference
+#'  between sap flow probes under zero-flow conditions, in degrees Celsius)
+#'  time series. The length of the vector must match that of `vctr_dt`. Missing
+#'  values must be gap-filled previously.
+#' @param alpha A positive value representing a multiplier in the equation to
+#'  calculate Fd. Default is 1.19 * 10^(-4) (m3 m-2 s-1).
+#' @param beta A positive value representing a power in the equation to
+#'  calculate Fd. Default is 1.231.
+#' @param do_heartwood_correction A boolean. If `TRUE`, the heartwood
+#'  correction is applied to correct dT before calculating Fd; else, the
+#'  correction is not applied. Default is `FALSE`.
+#' @param ratio_conductive A number between 0 and 1, indicating the ratio of
+#'  the probe length to sapwood width. This parameter must be provided if
+#'  `do_heartwood_correction` is `TRUE`. Default is `NULL`.
+#'
+#' @returns
+#' A vector of Fd (m3 m-2 s-1). The length of the vector matches that of the
+#'  input dT and dTmax vectors.
 #'
 #' @examples
+#' ## Load data
 #' data(dt_gf)
 #' data(dtmax)
-#'
 #' dt <- dt_gf$dt
 #' dtmax <- dtmax$dtmax_sp
 #'
-#' # Calculate Fd from data
+#' ## Calculate Fd
 #' result <- calc_fd(dt, dtmax)
 #'
-#' @include utils.R utils-pipe.R
+#' @author Yoshiaki Hata
+#'
+#' @include utils.R
+#'
 #' @export
 
 calc_fd <-
-  function(vctr_dt, vctr_dtmax, alpha = 1.19 * 10^{-4}, beta = 1.231,
+  function(vctr_dt, vctr_dtmax, alpha = 1.19 * 10^(-4), beta = 1.231,
            do_heartwood_correction = FALSE, ratio_conductive = NULL) {
     ## avoid "No visible binding for global variable" notes
     dt <- NULL
@@ -37,6 +62,16 @@ calc_fd <-
     fd <- NULL
 
     ## check input values
+    if(do_heartwood_correction == TRUE & is.null(ratio_conductive)) {
+      stop("ratio_conductive must be provided to do heartwood correction")
+    }
+
+    if(do_heartwood_correction == TRUE & !is.null(ratio_conductive)) {
+      if(ratio_conductive < 0 | ratio_conductive > 1) {
+        stop("ratio_conductive must be between 0 and 1")
+      }
+    }
+
     data_all <- data.frame(dt = vctr_dt,
                            dtmax = vctr_dtmax)
 
@@ -59,3 +94,4 @@ calc_fd <-
 
     return(vctr_fd)
   }
+
