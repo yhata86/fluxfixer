a65d7bb3207ce40ad472ba73a0e3fe57a09d6439
yhata86 <yoshiaki.hata0806@gmail.com>
2026-01-11 17:11:00 +0900

    added explanation

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,17 @@
-#' Obtain time interval of input time vector
+#' Obtain time interval of input timestamp vector
 #'
-#' @description `get_interval()` retrieves time interval of input time vector
-#'   and checks whether the format of the time vector is acceptable for
-#'   the successive process.
+#' @description `get_interval()` retrieves the time interval of the input
+#'  timestamp vector and checks whether the format of the time vector is
+#'  acceptable for the successive process.
 #'
-#' @param vctr_time Timestamp vector (POSIXct or POSIXt class)
+#' @param vctr_time A timestamp vector of class POSIXct or POSIXt. The
+#'  timestamps must be equally spaced and arranged chronologically.
+#'
+#' @returns
+#' A numeric (minutes) indicating the time interval of the input timestamp
+#'  vector.
+#'
+#' @author Yoshiaki Hata
 
 get_interval <- function(vctr_time) {
   if(lubridate::is.POSIXt(vctr_time) != TRUE) {
@@ -26,29 +33,46 @@ get_interval <- function(vctr_time) {
 }
 
 
-#' Obtain TOA global radiation time series
+#' Calculate global solar radiation time series at TOA
 #'
-#' @description `calc_global_radiation_toa()` calculates TOA (top of atmosphere)
-#'   global radiation time series at specific place based on Campbell and
-#'   Norman (1998)
+#' @description `calc_sw_in_toa()` obtains incident global solar radiation
+#'  time series at TOA (top of atmosphere) at a specific location by
+#'  calculating solar elevation angle estimated from the equations of Campbell
+#'  and Norman (1998).
 #'
-#' @param vctr_time Timestamp vector (POSIXct or POSIXt class).
-#' @param lat Latitude (unit: degree).
-#' @param lon Longitude (unit: degree).
-#' @param std_meridian Standard meridian (unit: degree).
-#' @param solar_const Solar constant (unit: W/m2).
-#' @param sbeta_min Minimum solar elevation angle (unit: degree).
-#'   If the calculated solar elevation angle is less than this threshold,
-#'   the corresponding TOA global radiation becomes zero.
+#' @inheritParams get_interval
+#' @param lat A numeric value (degrees) between -90 and 90, indicating the
+#'  latitude of the specific location.
+#' @param lon A numeric value (degrees) between -180 and 180, indicating the
+#'  longitude of the specific location.
+#' @param std_meridian A numeric value (degrees) between -180 and 180,
+#'  indicating the standard meridian of the specific location.
+#' @param solar_const A positive value (W m-2) indicating the solar constant.
+#'  Default is 1365 (W m-2).
+#' @param sbeta_min A threshold value of the solar elevation angle (degrees).
+#'  If the calculated solar elevation angle is less than this threshold, the
+#'  corresponding global solar radiation becomes zero.
+#'
+#' @returns
+#' A vector of the global solar radiation at TOA (W m-2). The length of the
+#' vector matches that of the input timestamp vector.
+#'
 #' @examples
-#'   calc_global_radiation_toa(seq(as.POSIXct("2025/01/01"),
-#'                                 as.POSIXct("2025/01/02"), by = "30 min"),
-#'                             4.33, 133.83, 120)
+#' ## Make a timestamp vector
+#' timezone <- "Etc/GMT-8"
+#' time <- seq(as.POSIXct("2026/01/01", tz = timezone),
+#'  as.POSIXct("2026/01/02", tz = timezone), by = "30 min")
+#'
+#' ## Obtain global solar radiation at Lambir Hills National Park in Malaysia
+#' result <- calc_sw_in_toa(time, 4.201007, 114.039079, 120)
+#'
+#' @author Yoshiaki Hata
 #'
 #' @export
 
-calc_global_radiation_toa <- function(vctr_time, lat, lon, std_meridian,
-                                      solar_const = 1365, sbeta_min = 0.001) {
+calc_sw_in_toa <-
+  function(vctr_time, lat, lon, std_meridian, solar_const = 1365,
+           sbeta_min = 0.001) {
   DOY <- lubridate::yday(vctr_time)
   HourMin <- lubridate::hour(vctr_time) + lubridate::minute(vctr_time) / 60
 
@@ -79,8 +103,15 @@ calc_global_radiation_toa <- function(vctr_time, lat, lon, std_meridian,
 #' @description `n_valid()` retrieves the number of data points without missing
 #'   values.
 #'
-#' @param vctr Vector to be evaluated
-#' @param label_err Error value label representing missing value
+#' @param vctr A vector to be evaluated.
+#' @param label_err A numeric value representing a missing value in the input
+#'  vector. Default is -9999.
+#'
+#' @returns
+#' An integer indicating the number of the input vector elements without
+#'  missing values.
+#'
+#' @author Yoshiaki Hata
 
 n_valid <-
   function(vctr, label_err = -9999) {
@@ -89,22 +120,74 @@ n_valid <-
   }
 
 
-#' Check small signal dent by peak position
+#' Detect periods when short-term signal attenuation occurs
 #'
-#' @description `check_small_dent()` detects small signal dent periods
-#'   by peak position.
+#' @description `check_short_attenuation()` detects periods of temporary signal
+#'  attenuation by the upward (or downward) peak position of the smoothed
+#'  average (or standard deviation) time series.
 #'
-#' @param vctr_time Vector to be evaluated
-#' @param vctr_dt_z Vector to be evaluated
-#' @param vctr_dt_avg Vector to be evaluated
-#' @param vctr_dt_sd Vector to be evaluated
-#' @param wndw_size_dent Integer determining the moving window size
-#' @param inv_sigma Numerical value for Gaussian window
-#' @param label_err Error value label representing missing value
+#' @details
+#' In some cases, the dT time series may yield a signal that is attenuated for
+#' only a short period, for example, when rainfall continues for days, causing
+#' the moving window mean (or standard deviation) to increase (or decrease).
+#' In such cases, normalization will cause the Z-score time series immediately
+#' before and after the rainfall to be unnaturally distorted, hindering the
+#' construction of the random-forest model. This function can detect periods
+#' when the moving window average has an upward peak, and the moving window
+#' standard deviation has a downward peak simultaneously.
+#'
+#' First, the average and standard deviation time series are interpolated if
+#' they contain missing values. Second, they are smoothed by convolution with a
+#' Gaussian window specified by the user through the parameters
+#' `wndw_size_conv` and `inv_sigma_conv`. Third, the first-order and
+#' second-order differences of both smoothed time series are calculated, which
+#' determine the upward peak positions of the average and the downward peak
+#' positions of the standard deviation. Finally, possible signal attenuation
+#' periods are output. The start and end of the periods are defined by the
+#' timings when the first-order differenced standard deviation time series
+#' changes its sign before and after each peak.
+#'
+#' @inheritParams calc_dtmax_sp
+#' @inheritParams n_valid
+#' @param vctr_z A vector of Z-score time series. The length of the vector must
+#'  match that of the timestamp vector.
+#' @param vctr_avg A vector of moving window average time series that used in
+#'  transforming the original time series into the Z-score time series. The
+#'  length of the vector must match that of the timestamp vector. The unit of
+#'  the time series must match that of `vctr_sd`.
+#' @param vctr_sd A vector of moving window standard deviation time series that
+#'  used in transforming the original time series into the Z-score time series.
+#'  The length of the vector must match that of the timestamp vector. The unit
+#'  of the time series must match that of `vctr_avg`.
+#' @param wndw_size_conv An integer indicating the number of data points
+#'  included in a moving window. Default is 48 * 15, implying that the window
+#'  size is 15 days if the time interval of the input timestamp is 30 minutes.
+#' @param inv_sigma_conv A positive value defining a Gaussian window width. The
+#'  width of the Gaussian window is inversely proportional to this parameter.
+#'  Default is 0.01.
+#'
+#' @returns
+#' A data frame with columns below:
+#'
+#' * The first column, `time_start`, gives the timestamp at which the detected
+#'  attenuation period begins.
+#'
+#' * The second column, `time_peak`, gives the timestamp of the detected
+#'  attenuation period peak.
+#'
+#' * The third column, `time_end`, gives the timestamp at which the detected
+#'  attenuation period ends.
+#'
+#' * The fourth column, `ratio`, gives the average of the ratio of the standard
+#'  deviation at the detected attenuation peak to that at the beginning and end
+#'  of the attenuation period. This value can be used as a threshold for
+#'  determining the final attenuation periods that require Z-score modification.
+#'
+#' @author Yoshiaki Hata
 
-check_small_dent <-
-  function(vctr_time, vctr_dt_z, vctr_dt_avg, vctr_dt_sd,
-           wndw_size_dent = 720, inv_sigma = 0.01, label_err = -9999) {
+check_short_attenuation <-
+  function(vctr_time, vctr_z, vctr_avg, vctr_sd, wndw_size_conv = 48 * 15,
+           inv_sigma_conv = 0.01, label_err = -9999) {
     ## avoid "No visible binding for global variable" notes
     time <- NULL
     dT_z <- NULL
@@ -130,9 +213,9 @@ check_small_dent <-
 
     data <-
       data.frame(time = vctr_time,
-                 dT_z = vctr_dt_z,
-                 dT_avg = vctr_dt_avg,
-                 dT_sd = vctr_dt_sd) %>%
+                 dT_z = vctr_z,
+                 dT_avg = vctr_avg,
+                 dT_sd = vctr_sd) %>%
       dplyr::na_if(label_err)
 
     time_mea_start <-
@@ -150,7 +233,7 @@ check_small_dent <-
     interval_time <- vctr_time %>% diff() %>% as.numeric() %>% max()
 
     ## Gaussian filter
-    g_filter <- gsignal::gaussian(wndw_size_dent, inv_sigma)
+    g_filter <- gsignal::gaussian(wndw_size_conv, inv_sigma_conv)
     g_filter <- g_filter / sum(g_filter)
 
     data <-
@@ -165,13 +248,8 @@ check_small_dent <-
                     dT_avg_smth_diff1 = dT_avg_smth - stats::lag(dT_avg_smth),
                     dT_avg_smth_diff2 = dT_avg_smth_diff1 -
                       stats::lag(dT_avg_smth_diff1),
-                    # flag_dT_avg_peak = ifelse(dT_avg_smth_diff1 == 0, 1, 0),
-                    # flag_dT_avg_peak =
-                    #   ifelse(dT_avg_smth_diff1 *
-                    #            dplyr::lead(dT_avg_smth_diff1) < 0,
-                    #          1, flag_dT_avg_peak),
                     flag_dT_avg_peak =
-                      ifelse(dT_avg_smth_diff1 == 0 &
+                      ifelse(dT_avg_smth_diff1 == 0 |
                                dT_avg_smth_diff1 *
                                dplyr::lead(dT_avg_smth_diff1) < 0, 1, 0),
                     dT_sd_smth = gsignal::conv(dT_sd_smth, g_filter,
@@ -179,13 +257,8 @@ check_small_dent <-
                     dT_sd_smth_diff1 = dT_sd_smth - stats::lag(dT_sd_smth),
                     dT_sd_smth_diff2 = dT_sd_smth_diff1 -
                       stats::lag(dT_sd_smth_diff1),
-                    # flag_dT_sd_peak = ifelse(dT_sd_smth_diff1 == 0, 1, 0),
-                    # flag_dT_sd_peak =
-                    #   ifelse(dT_sd_smth_diff1 *
-                    #            dplyr::lead(dT_sd_smth_diff1) < 0,
-                    #          1, flag_dT_sd_peak),
                     flag_dT_sd_peak =
-                      ifelse(dT_sd_smth_diff1 == 0 &
+                      ifelse(dT_sd_smth_diff1 == 0 |
                                dT_sd_smth_diff1 *
                                dplyr::lead(dT_sd_smth_diff1) < 0, 1, 0))
 
@@ -206,41 +279,41 @@ check_small_dent <-
       dplyr::pull(time)
 
     if(length(vctr_time_sd_dpeak) <= 1) {
-      message("--- There are less than two peaks. Skip dent detection.")
+      message("--- There are less than two peaks. Skip the detection.")
       data.frame(time_start = vctr_time_start,
                  time_peak = vctr_time_peak,
                  time_end = vctr_time_end,
                  ratio = vctr_ratio) %>%
         return()
     } else {
-      ## detect possible damping periods
+      ## detect possible attenuation periods
       for (i in 1:length(vctr_time_sd_dpeak)) {
         target_time_sd_dpeak <- vctr_time_sd_dpeak[i]
         num_time_sd_peak <- which(vctr_time_sd_peak == target_time_sd_dpeak)
 
       if(num_time_sd_peak == 1) {
-          damp_head <- time_mea_start + interval_time
-          damp_tail <- vctr_time_sd_peak[num_time_sd_peak + 1] - interval_time
+          attn_head <- time_mea_start + interval_time
+          attn_tail <- vctr_time_sd_peak[num_time_sd_peak + 1] - interval_time
         } else if(num_time_sd_peak == length(vctr_time_sd_peak)) {
-          damp_head <- vctr_time_sd_peak[num_time_sd_peak - 1] + interval_time
-          damp_tail <- time_mea_end - interval_time
+          attn_head <- vctr_time_sd_peak[num_time_sd_peak - 1] + interval_time
+          attn_tail <- time_mea_end - interval_time
         } else {
-          damp_head <- vctr_time_sd_peak[num_time_sd_peak - 1] + interval_time
-          damp_tail <- vctr_time_sd_peak[num_time_sd_peak + 1] - interval_time
+          attn_head <- vctr_time_sd_peak[num_time_sd_peak - 1] + interval_time
+          attn_tail <- vctr_time_sd_peak[num_time_sd_peak + 1] - interval_time
         }
 
         num_time_avg_peak <-
-          which(vctr_time_avg_upeak >= damp_head &
-                  vctr_time_avg_upeak <= damp_tail)
+          which(vctr_time_avg_upeak >= attn_head &
+                  vctr_time_avg_upeak <= attn_tail)
 
-        df_damp <-
+        df_attn <-
           data %>%
-          dplyr::filter(time >= damp_head & time <= damp_tail)
+          dplyr::filter(time >= attn_head & time <= attn_tail)
 
-        dT_sd_head <- dplyr::slice_head(df_damp) %>% dplyr::pull(dT_sd_smth)
-        dT_sd_tail <- dplyr::slice_tail(df_damp) %>% dplyr::pull(dT_sd_smth)
+        dT_sd_head <- dplyr::slice_head(df_attn) %>% dplyr::pull(dT_sd_smth)
+        dT_sd_tail <- dplyr::slice_tail(df_attn) %>% dplyr::pull(dT_sd_smth)
         dT_sd_peak <-
-          df_damp %>%
+          df_attn %>%
           dplyr::filter(., time == target_time_sd_dpeak) %>%
           dplyr::pull(dT_sd_smth)
 
@@ -249,9 +322,9 @@ check_small_dent <-
                 digits = 5)
 
         if(length(num_time_avg_peak) >= 1) {
-          vctr_time_start <- c(vctr_time_start, damp_head)
+          vctr_time_start <- c(vctr_time_start, attn_head)
           vctr_time_peak <- c(vctr_time_peak, target_time_sd_dpeak)
-          vctr_time_end <- c(vctr_time_end, damp_tail)
+          vctr_time_end <- c(vctr_time_end, attn_tail)
           vctr_ratio <- c(vctr_ratio, dT_sd_peak_ratio)
         }
       }
@@ -268,5 +341,4 @@ check_small_dent <-
         return()
     }
   }
-
 
